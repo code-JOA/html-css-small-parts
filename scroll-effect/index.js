7bff368100fb48c2678edf22cbbc6467e349cd43
Joshua Owusu <joshbillion1@gmail.com>
2023-06-13 15:32:22 +0000

    Commit auto_update

--- scroll-effect/index.js
+++ scroll-effect/index.js
@@ -11,5 +11,8 @@ const handleOnMove = (e) => {
     if (track.dataset.mouseDownAt === '0') return;
 
     const mouseDelta = parseFLoat(track.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth
+    maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained = 
 }
