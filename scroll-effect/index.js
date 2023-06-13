9035a381b303cde50bc42357ad031af98f63208d
Joshua Owusu <joshbillion1@gmail.com>
2023-06-13 15:30:14 +0000

    Commit auto_update

--- scroll-effect/index.js
+++ scroll-effect/index.js
@@ -4,5 +4,9 @@ const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
 const handleOnUp = (e) => {
     track.dataset.mouseDownAt = '0'
-    
+    track.dataset.prevPercentage = track.dataset.percentage;
 };
+
+const handleOnMove = (e) => {
+    if (track.dataset.mouse)
+}
