664b4df4e2a0a528be92c9ef318be61107790b2a
Joshua Owusu <joshbillion1@gmail.com>
2023-06-13 16:27:23 +0000

    Commit auto_update

--- scroll-effect/script.js
+++ scroll-effect/script.js
@@ -3,17 +3,17 @@ const track = document.getElementById("image-track");
 const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
 const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
 };
 
 const handleOnMove = (e) => {
-    if (track.dataset.mouseDownAt === "0") return;
-    
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+  if (track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
     maxDelta = window.innerWidth / 2;
-    
-    const percentage = (mouseDelta / maxDelta) * -100,
+
+  const percentage = (mouseDelta / maxDelta) * -100,
     nextPercentageUnconstrained =
       parseFloat(track.dataset.prevPercentage) + percentage,
     nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
