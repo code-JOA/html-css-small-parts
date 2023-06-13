8b686084720a67057ac931f2519344402bbecff7
Joshua Owusu <joshbillion1@gmail.com>
2023-06-13 15:34:31 +0000

    Commit auto_update

--- scroll-effect/index.js
+++ scroll-effect/index.js
@@ -16,5 +16,13 @@ const handleOnMove = (e) => {
     const percentage = (mouseDelta / maxDelta) * -100,
     nextPercentageUnconstrained = 
     parseFloat(track.dataset.prevPercentage) + percentage,
-    nextPercentage = Math.max(Math.min(nextPercentage))
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate(
+        {
+            transform: `translate()`
+        }
+    )
 }
