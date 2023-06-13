58e89e38da1495f7cf5a3f2a67d30259fe4de810
Joshua Owusu <joshbillion1@gmail.com>
2023-06-13 15:29:10 +0000

    Commit auto_update

--- scroll-effect/index.js
+++ scroll-effect/index.js
@@ -1,3 +1,8 @@
 const track = document.getElementById('image-track');
 
-const handleOnDown = (e) => (track.dataset.mouseDownAt = e.client)
+const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
+
+const handleOnUp = (e) => {
+    track.dataset.mouseDownAt = '0'
+    
+};
